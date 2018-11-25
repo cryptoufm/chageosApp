e8b99ffbf9c26b178b485211b2f0c1411ea8bb61
jbagur <jbagur@ufm.edu>
2018-11-25 13:16:50 -0600

    Json POST

--- app/src/main/java/com/example/julio/miprimerapp/MainActivity.java
+++ app/src/main/java/com/example/julio/miprimerapp/MainActivity.java
@@ -81,15 +81,12 @@ public class MainActivity extends AppCompatActivity {
                 builder.setTitle("Registro de voto");
                 builder.setMessage("Voluntarix:  "+mySpinner.getSelectedItem().toString()+
                 "\nCUI:  "+CUIText.getText().toString());
-//                System.out.println("Voluntarix:  "+mySpinner.getSelectedItem().toString());
-//                System.out.println("Prueba>"+imgimagen.getDrawable());
-//                System.out.println("CUI:  "+CUIText.getText().toString());
-//                Bitmap bm=((BitmapDrawable)imgimagen.getDrawable()).getBitmap();
-//                System.out.println("Bitmap:  "+bm);
- //               ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//                bm.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
-//                byte[] byteArray = byteArrayOutputStream .toByteArray();
-   //             final String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                Bitmap bm=((BitmapDrawable)imgimagen.getDrawable()).getBitmap();
+                System.out.println("Bitmap:  "+bm);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bm.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream .toByteArray();
+                final String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
 //                System.out.println("Encoded:  "+encoded);
 
                 builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -107,12 +104,33 @@ public class MainActivity extends AppCompatActivity {
                             public void run() {
                                 try {
 
+                                    URL url = new URL("http://35.211.207.73");
+                                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                                    conn.setRequestMethod("POST");
+                                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                                    conn.setRequestProperty("Accept","application/json");
+                                    conn.setDoOutput(true);
+                                    conn.setDoInput(true);
+
                                     JSONObject jsonParam = new JSONObject();
-                                    jsonParam.put("Voluntario",mySpinner.getSelectedItem().toString());
-                                    jsonParam.put("CUI",CUIText.getText().toString());
-                                    //jsonParam.put("Photo", encoded);
+
+                                    jsonParam.put("image_hash", encoded);
+                                    jsonParam.put("citizenuid",CUIText.getText().toString());
+                                    jsonParam.put("volunteer_id",mySpinner.getSelectedItem().toString());
 
                                     Log.i("JSON: ", jsonParam.toString());
+
+                                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                                    os.writeBytes(jsonParam.toString());
+
+                                    os.flush();
+                                    os.close();
+
+                                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                                    Log.i("MSG" , conn.getResponseMessage());
+
+                                    conn.disconnect();
 
                                 } catch (Exception e) {
                                     e.printStackTrace();
